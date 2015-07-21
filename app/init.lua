bbd0f5ad0c93b3fa114b6a144ef267b8be4e0798
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2015-07-20 23:32:15 -0300

    improved solution

--- app/init.lua
+++ app/init.lua
@@ -5,19 +5,20 @@ local camera = Camera:new()
 local server = Pegasus:new()
 
 local videoFeed = function(req, rep)
-  local isIndex = string.find(req.path, '/') ~= nil
+  local isIndex = string.find(req.path, '/video/') == nil
 
   if isIndex then
     print('index')
-    rep:addHeader('Content-Type', 'text/html'):write('<img src="http://localhost:9090/video-feed/">')
+    rep:addHeader('Content-Type', 'text/html'):write('<img src="http://localhost:9090/video/">')
     return nil
   end
 
   print('video-feed')
   rep:addHeader('Content-Type', 'multipart/x-mixed-replace; boundary=frame')
 
-  for i=1, 3 do
-    local frame = Camera:getFrame(i)
+  for index=2, 3 do
+    local frame = Camera:getFrame(index)
+    print(index)
     local src = table.concat({
       '--frame\r\n',
       'Content-Type: image/jpeg\r\n\r\n' .. frame .. '\r\n'
